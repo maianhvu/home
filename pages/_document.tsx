c220259e30e0758e13ebb53c2510c5ba86ac2373
Mai Anh Vu <me@maianhvu.com>
2022-02-13 00:13:02 +0800

    Update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;500&family=Inter:wght@400;600&family=Space+Mono&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍣</text></svg>"
+          />
         </Head>
         <body className="bg-white dark:bg-black text-gray-500 font-mono">
           <Main />
